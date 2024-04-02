16c3d1fa5301637fdaaf1c36c99ca2662720ad7f
The Huong <huongtn300902@gmail.com>
2024-04-02 20:33:12 +0700

    thehuong first commit

---
+++ src/main/java/com/thehuong/royalhotel/RoyalHotelApplication.java
@@ -0,0 +1,13 @@
+package com.thehuong.royalhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoyalHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thehuong/royalhotel/RoyalHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thehuong.royalhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoyalHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
